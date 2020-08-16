773741ed7d6aa517b269a88562a552a068c0a4a0
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-16 20:04:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
-  if word.map(&:first).scan(/\w[aeiou]/) 
-    return true 
+  if word.map(&:first).scan(/\w[aeiou]/)
+    return true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
