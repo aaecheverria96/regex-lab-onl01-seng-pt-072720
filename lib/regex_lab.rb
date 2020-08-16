aab6c0b273bf3d97e435a228d36064738a10f565
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-16 20:15:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,9 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+five_letters = []
 text.grep(/^\w{5}\s/)
+return five_letters
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
